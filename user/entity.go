5de83ff8a497dcf2f8395fe4db9f4caa7e386a52
I Komang Setiana <komangsetiana40@gmail.com>
2023-11-04 10:47:23 +0800

    get campaign formatter

--- user/entity.go
+++ user/entity.go
@@ -1,16 +1,17 @@
 package user
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
-	ID               int
-	Name             string
-	Occupation       string
-	Email            string
-	PasswordHash     string
-	AvatarFileName   string
-	Role             string
-	CreatedAt       time.Time
-	UpdatedAt		time.Time
-
+	ID             int
+	Name           string
+	Occupation     string
+	Email          string
+	PasswordHash   string
+	AvatarFileName string
+	Role           string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
